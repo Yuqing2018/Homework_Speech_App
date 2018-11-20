64671f98986e7c107b81e15d15b9ace0c4ad2b99
yuqing <v-yuqw@microsoft.com>
2018-11-20 14:27:51 +0800

    upload soucre code

---
+++ Homework_Speech_App/Homework_Speech_App/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace Homework_Speech_App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<string> AudioFormatList = new List<string>()
+        {
+            "PCM","WAV","AMR"
+        };
+        public MainWindow()
+        {
+            InitializeComponent();
+            audioFormat_CB.ItemsSource = Enum.GetValues(typeof(AudioFormatType)).Cast<AudioFormatType>();
+            audioFormat_CB.SelectedValue = AudioFormatType.PCM;
+        }
+
+        private void SelectFile_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            OpenFileDialog dlg = new OpenFileDialog();
+
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".pcm";
+            dlg.Filter = "Speech Files (*.pcm,*.wav,*.amr)|*.pcm;*.wav;*.amr;*.PCM;*.WAV;*.AMR";
+            
+            // Display OpenFileDialog by calling ShowDialog method 
+            bool? result = dlg.ShowDialog();
+            
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                filePath_TB.Text = dlg.FileName;
+            }
+
+        }
+
+        private void StartRecognition_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            bool flag = true;
+            var filePath = this.filePath_TB.Text;
+            var audioFormat = (AudioFormatType)audioFormat_CB.SelectedValue;
+            if (string.IsNullOrEmpty(filePath))
+            {
+                flag = false;
+                MessageBox.Show("请选择识别文件!");
+            }
+            else
+            {
+                int startIndex = filePath.LastIndexOf('.');
+                int extlength = filePath.Length - filePath.LastIndexOf('.') - 1;
+                var fileExt = filePath.Substring(startIndex + 1, extlength);
+                if (audioFormat.ToString().ToLower() != fileExt.ToLower())
+                {
+                    flag = false;
+                    MessageBox.Show("请选择音频格式!");
+                }
+            }
+            if (flag)
+            {
+                var result = SpeechHelper.AsrData(filePath, audioFormat.ToString().ToLower());
+                var speechResult = JsonConvert.DeserializeObject<SpeechModel>(result.ToString());
+
+                //RecognitionResults_RTB.Document.Blocks.Clear();
+                speechResult.result.ForEach(item =>
+                {
+                    Run runx = new Run(item);
+                    Paragraph paragraph = new Paragraph(runx);
+                    RecognitionResults_RTB.Document.Blocks.Add(paragraph);
+                });
+            }
+        }
+
+        private void StartSynthesis_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            TextRange textRange = new TextRange(TextToSynthesis_Rtb.Document.ContentStart, TextToSynthesis_Rtb.Document.ContentEnd);
+            var result = SpeechHelper.Tts(textRange.Text);
+            MessageBox.Show("合成成功！");
+
+        }
+
+    }
+
+    public enum AudioFormatType
+    {
+        PCM,
+        WAV,
+        AMR
+    }
+}
---
+++ Homework_Speech_App/Homework_Speech_App/SpeechHelper.cs
@@ -0,0 +1,84 @@
+﻿using Baidu.Aip.Speech;
+using Microsoft.Win32;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Homework_Speech_App
+{
+    public class SpeechHelper
+    {
+        // 设置APPID/AK/SK
+        private const string APP_ID = "14489880";
+        private const string API_KEY = "8Sr9u9XYkRPcA62wL02aC43V";
+        private const string SECRET_KEY = "i0QtdyawLje2mZEV9kWU5dbQqw8Y76ob";
+
+        private static Asr _asrClient = new Asr(API_KEY, SECRET_KEY) { Timeout = 6000};
+        private static Tts _ttsClient = new Tts(API_KEY, SECRET_KEY) { Timeout = 6000 };
+        /// <summary>
+        /// 通过文件路径，调用百度语音识别接口返回识别结果
+        /// </summary>
+        /// <param name="filePath">文件路径</param>
+        /// <param name="audioFormat">音频格式</param>
+        /// <returns></returns>
+        public static JObject AsrData(string filePath,string audioFormat)
+        {
+            try
+            {
+                var data = File.ReadAllBytes(filePath);
+                _asrClient.Timeout = 120000; // 若语音较长，建议设置更大的超时时间. ms
+                var result = _asrClient.Recognize(data, audioFormat, 16000);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+
+        // 合成
+        public static bool Tts(string content)
+        {
+            try
+            {
+                // 可选参数
+                var option = new Dictionary<string, object>()
+                {
+                    {"spd", 5}, // 语速
+                    {"vol", 7}, // 音量
+                    {"per", 4}  // 发音人，4：情感度丫丫童声
+                };
+                var result = _ttsClient.Synthesis(content, option);
+
+                if (result.ErrorCode == 0)  // 或 result.Success
+                {
+                    SaveFile(result.Data);
+                }
+                return result.Success;
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+
+        public static void SaveFile(byte[] outputBytes)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+                File.WriteAllBytes(saveFileDialog.FileName, outputBytes);
+        }
+
+    }
+
+    public class SpeechModel
+    {
+        public string err_no { get; set; }
+        public string err_msg { get; set; }
+        public string corpus_no { get; set; }
+        public string sn { get; set; }
+        public List<string> result { get; set; }
+    }
+}
+
